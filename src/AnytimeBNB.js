d513a3f4053d35b9456c32662c56cd42a37c2b20
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-04-02 15:38:00 +0500

    anytime bnb

--- src/AnytimeBNB.js
+++ src/AnytimeBNB.js
@@ -4,82 +4,97 @@ const AnytimeBNB = () => {
   return (
     <div className="anytime_bnb_main_div">
       <div className="anytime_bnb_detail_div_1">
-        <div className="number_Div">1</div>
-        <div className="anytime_left_right">
-            <div className="anytime_left">
-                <p>Daily Earning</p>
-                <h2>10.6%</h2>
-                <br />
-                <p>Withdraw Anytime</p>
-                <h2>Anytime</h2>
-                <br />
-                <p>Enter Amount</p>
-                <input type="text" placeholder="5" />
-            </div>
-            <div className="anytime_right">
-                <p>Total ROI</p>
-                <h2>148.4%</h2>
-                <br />
-                <p>Days</p>
-                <h2>14</h2>
-                <br />
-                <p>ROI 14 Days</p>
-                <h2>0.00</h2>
-            </div>
+        <div className="number_Div">
+          <span>1</span>
         </div>
-        <button>STAKE BNB</button>
+        <div className="anytime_left_right">
+          <div className="anytime_left">
+            <span>Daily Earning</span>
+            <h2>10.6%</h2>
+            <br />
+            <span>Withdraw Anytime</span>
+            <h2>Anytime</h2>
+            <br />
+            <span>Enter Amount</span>
+            <br />
+            <input type="text" placeholder="5" />
+          </div>
+          <div className="anytime_right">
+            <span>Total ROI</span>
+            <h2>148.4%</h2>
+            <br />
+            <span>Days</span>
+            <h2>14</h2>
+            <br />
+            <span>ROI 14 Days</span>
+            <h2>0.00</h2>
+          </div>
+        </div>
+        <div className="stake_btn">
+          <button>STAKE BNB</button>
+        </div>
       </div>
-      <div className="anytime_bnb_detail_div_2">
-        <div className="number_Div">1</div>
-        <div className="anytime_left_right">
-            <div className="anytime_left">
-                <p>Daily Earning</p>
-                <h2>9.1%</h2>
-                <br />
-                <p>Withdraw Anytime</p>
-                <h2>Anytime</h2>
-                <br />
-                <p>Enter Amount</p>
-                <input type="text" placeholder="5" />
-            </div>
-            <div className="anytime_right">
-                <p>Total ROI</p>
-                <h2>191.1%</h2>
-                <br />
-                <p>Days</p>
-                <h2>21</h2>
-                <br />
-                <p>ROI 21 Days</p>
-                <h2>0.00</h2>
-            </div>
+      <div className="anytime_bnb_detail_div_1">
+        <div className="number_Div">
+          <span>2</span>
         </div>
-        <button>STAKE BNB</button>
+        <div className="anytime_left_right">
+          <div className="anytime_left">
+            <span>Daily Earning</span>
+            <h2>9.1%</h2>
+            <br />
+            <span>Withdraw Anytime</span>
+            <h2>Anytime</h2>
+            <br />
+            <span>Enter Amount</span>
+            <br />
+            <input type="text" placeholder="5" />
+          </div>
+          <div className="anytime_right">
+            <span>Total ROI</span>
+            <h2>191.1%</h2>
+            <br />
+            <span>Days</span>
+            <h2>21</h2>
+            <br />
+            <span>ROI 21 Days</span>
+            <h2>0.00</h2>
+          </div>
+        </div>
+        <div className="stake_btn">
+          <button>STAKE BNB</button>
+        </div>
       </div>
-      <div className="anytime_bnb_detail_div_3">
-        <div className="number_Div">1</div>
-        <div className="anytime_left_right">
-            <div className="anytime_left">
-                <p>Daily Earning</p>
-                <h2>7.6%</h2>
-                <br />
-                <p>Withdraw Anytime</p>
-                <h2>Anytime</h2>
-                <br />
-                <p>Enter Amount</p>
-                <input type="text" placeholder="5" />
-            </div>
-            <div className="anytime_right">
-                <p>Total ROI</p>
-                <h2>212.8%</h2>
-                <br />
-                <p>Days</p>
-                <h2>28</h2>
-                <br />
-                <p>ROI 28 Days</p>
-                <h2>0.00</h2>
-            </div>
+      <div className="anytime_bnb_detail_div_1">
+        <div className="number_Div">
+          <span>3</span>
         </div>
-        <button>STAKE BNB</button>
+        <div className="anytime_left_right">
+          <div className="anytime_left">
+            <span>Daily Earning</span>
+            <h2>7.6%</h2>
+            <br />
+            <span>Withdraw Anytime</span>
+            <h2>Anytime</h2>
+            <br />
+            <span>Enter Amount</span>
+            <br />
+            <input type="text" placeholder="5" />
+          </div>
+          <div className="anytime_right">
+            <span>Total ROI</span>
+            <h2>212.8%</h2>
+            <br />
+            <span>Days</span>
+            <h2>28</h2>
+            <br />
+            <span>ROI 28 Days</span>
+            <h2>0.00</h2>
+          </div>
+        </div>
+        <div className="stake_btn">
+          <button>STAKE BNB</button>
+        </div>
       </div>
     </div>
   );
