0f75014fc114de3d8f0b5f4c6aee1be71e257c13
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-04-02 15:53:17 +0500

    locked bnb css

--- src/LockedBNB.js
+++ src/LockedBNB.js
@@ -3,83 +3,89 @@ import React from "react";
 const AnytimeBNB = () => {
   return (
     <div className="locked_bnb_main_div">
-      <div className="locked_bnb_detail_div_4">
-        <div className="number_Div">1</div>
+      <div className="locked_bnb_detail_div_1">
+        <div className="number_locked_Div"><span>4</span></div>
         <div className="locked_left_right">
           <div className="locked_left">
-            <p>Daily Earning</p>
+            <span>Daily Earning</span>
             <h2>10.6%</h2>
             <br />
-            <p>Withdraw Anytime</p>
+            <span>Withdraw Anytime</span>
             <h2>Locked</h2>
             <br />
-            <p>Enter Amount</p>
+            <span>Enter Amount</span>
             <input type="text" placeholder="5" />
           </div>
           <div className="locked_right">
-            <p>Total ROI</p>
+            <span>Total ROI</span>
             <h2>309.8%</h2>
             <br />
-            <p>Days</p>
+            <span>Days</span>
             <h2>14</h2>
             <br />
-            <p>ROI 14 Days</p>
+            <span>ROI 14 Days</span>
             <h2>0.00</h2>
           </div>
         </div>
-        <button>STAKE BNB</button>
+        <div className="stake_locked_btn">
+          <button>STAKE BNB</button>
+        </div>
       </div>
-      <div className="locked_bnb_detail_div_5">
-        <div className="number_Div">1</div>
+      <div className="locked_bnb_detail_div_1">
+      <div className="number_locked_Div"><span>5</span></div>
         <div className="locked_left_right">
           <div className="locked_left">
-            <p>Daily Earning</p>
+            <span>Daily Earning</span>
             <h2>9.1%</h2>
             <br />
-            <p>Withdraw Anytime</p>
+            <span>Withdraw Anytime</span>
             <h2>Locked</h2>
             <br />
-            <p>Enter Amount</p>
+            <span>Enter Amount</span>
             <input type="text" placeholder="5" />
           </div>
           <div className="locked_right">
-            <p>Total ROI</p>
+            <span>Total ROI</span>
             <h2>522.8%</h2>
             <br />
-            <p>Days</p>
+            <span>Days</span>
             <h2>21</h2>
             <br />
-            <p>ROI 21 Days</p>
+            <span>ROI 21 Days</span>
             <h2>0.00</h2>
           </div>
         </div>
-        <button>STAKE BNB</button>
+        <div className="stake_locked_btn">
+          <button>STAKE BNB</button>
+        </div>
       </div>
-      <div className="locked_bnb_detail_div_6">
-        <div className="number_Div">1</div>
+      <div className="locked_bnb_detail_div_1">
+      <div className="number_locked_Div"><span>6</span></div>
         <div className="locked_left_right">
           <div className="locked_left">
-            <p>Daily Earning</p>
+            <span>Daily Earning</span>
             <h2>7.6%</h2>
             <br />
-            <p>Withdraw Anytime</p>
+            <span>Withdraw Anytime</span>
             <h2>Locked</h2>
             <br />
-            <p>Enter Amount</p>
+            <span>Enter Amount</span>
             <input type="text" placeholder="5" />
           </div>
           <div className="locked_right">
-            <p>Total ROI</p>
+            <span>Total ROI</span>
             <h2>677.6%</h2>
             <br />
-            <p>Days</p>
+            <span>Days</span>
             <h2>28</h2>
             <br />
-            <p>ROI 28 Days</p>
+            <span>ROI 28 Days</span>
             <h2>0.00</h2>
           </div>
         </div>
-        <button>STAKE BNB</button>
+        <div className="stake_locked_btn">
+          <button>STAKE BNB</button>
+        </div>
       </div>
     </div>
   );
